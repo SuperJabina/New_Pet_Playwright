869c2c3aca194f09bc39ed5db79223f7938a9804
Max Lorey <54179892+SuperJabina@users.noreply.github.com>
2025-05-07 19:47:08 +0300

    21

--- tests/test_web_tables.py
+++ tests/test_web_tables.py
@@ -19,5 +19,5 @@ class TestWebTables:
         # 1. Переход на страницу Web Tables
         webtable_page.open(AppRoute.WEB_TABLES)
         webtable_page.add_button.click()
-        assert 5 == 0, "Тест ожидаемо упал" # Запланированное падение теста
+        assert 5 == 5, "Тест ожидаемо упал" # Запланированное падение теста
         # webtable_page.page.wait_for_timeout(5000)
